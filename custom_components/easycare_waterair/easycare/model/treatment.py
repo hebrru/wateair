6a6208c7700d3377e67a96653528419560b8b6e1
hebrru <134215992+hebrru@users.noreply.github.com>
2026-04-07 18:08:11 +0200

    Add treatment model

---
+++ custom_components/easycare_waterair/easycare/model/treatment.py
@@ -0,0 +1,55 @@
+"""Class reprsenting pool data."""
+
+import json
+
+from dateutil import parser
+
+
+class Treatment:
+    """Class representing the pool alerts."""
+
+    def __init__(self, pool: json) -> None:
+        """Initilisation of the class."""
+        if pool is None:
+            pool = json.loads("{}")
+            self._is_filled = False
+        else:
+            self._is_filled = True
+        self._pool = pool
+
+        if "waterChemistryCorrectionProtocol" in pool:
+            if pool["waterChemistryCorrectionProtocol"] is not None:
+                date_treatment = (
+                    parser.parse(pool["waterChemistryCorrectionProtocol"]["date"])
+                    if "date" in pool["waterChemistryCorrectionProtocol"]
+                    else parser.parse(
+                        pool["waterChemistryCorrectionProtocol"][
+                            "lastPHOutOfControlAlertSentDate"
+                        ]
+                    )
+                )
+                self._treatment = {
+                    "value": pool["waterChemistryCorrectionProtocol"][
+                        "correctionProtocolType"
+                    ],
+                    "date": date_treatment,
+                }
+            else:
+                self._treatment = {}
+        else:
+            self._treatment = {}
+
+    @property
+    def is_filled(self) -> bool:
+        """Return True if the client object is filled."""
+        return self._is_filled
+
+    @property
+    def treatment_value(self) -> str:
+        """The notification value."""
+        return self._treatment.get("value", "None")
+
+    @property
+    def treatment_date(self) -> str:
+        """The notification date."""
+        return self._treatment.get("date", None)
