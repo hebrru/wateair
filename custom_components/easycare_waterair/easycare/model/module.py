d1256f1ff182ff02a14f21ac8a0cb30d4df2d7d1
hebrru <134215992+hebrru@users.noreply.github.com>
2026-04-07 18:07:39 +0200

    Add module model

---
+++ custom_components/easycare_waterair/easycare/model/module.py
@@ -0,0 +1,73 @@
+"""Class representing module object."""
+
+import json
+
+
+class Module:
+    """Class representing the module object."""
+
+    def __init__(self, module: json) -> None:
+        """Initilisation of the class."""
+        if module is None:
+            module = json.loads("{}")
+            self._is_filled = False
+        else:
+            self._is_filled = True
+
+        self._type = module["type"]
+        self._name = module["name"]
+        self._id = module["id"]
+        self._serial_number = module["serialNumber"]
+        self._image = module["customPhoto"]
+        self._number_of_inputs = module["numberOfInputs"]
+        self._battery_level = module["getBatteryLevel"]
+        self._static_pressure = 0
+        if self._type == "lr-pr":
+            self._static_pressure = module["inputs"][0]["poolPressureAlertsVariables"][
+                "staticPressure"
+            ]
+
+    @property
+    def is_filled(self) -> bool:
+        """Return True if the module object is filled."""
+        return self._is_filled
+
+    @property
+    def type(self) -> str:
+        """The module type."""
+        return self._type
+
+    @property
+    def name(self) -> str:
+        """The module name."""
+        return self._name
+
+    @property
+    def id(self) -> str:
+        """The module id."""
+        return self._id
+
+    @property
+    def serial_number(self) -> str:
+        """The module name."""
+        return self._serial_number
+
+    @property
+    def image(self) -> str:
+        """The module name."""
+        return self._image
+
+    @property
+    def battery_level(self) -> str:
+        """The module bettery_level."""
+        return self._battery_level
+
+    @property
+    def number_of_inputs(self) -> str:
+        """The module name."""
+        return self._number_of_inputs
+
+    @property
+    def static_pressure(self) -> str:
+        """The module name."""
+        return self._static_pressure
